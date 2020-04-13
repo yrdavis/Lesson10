8963117adcd09d00157e9e64d89bf1ca66ced31d
yrdavis <yrd1@hotmail.com>
2020-04-12 22:09:12 -0700

    File to test word to see if they are palindrome

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,45 @@
 package exercises;
 
+/*
+ * @author Yvonne Davis
+ * @version 1
+ * This program looks for words in a defined dictionary to see if they are palindromes.
+ * 
+ */
+import java.util.ArrayList;
 import java.util.List;
 
 import examples.FileHelper;
 
 public class Palindrome {
 
+	List<String> words = new ArrayList<String>();
+
+	void Dictionary() {
+		words = loadWords();
+	}
+
 	public List<String> loadWords() {
 		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
 	}
+
+	public boolean wordExists(String word) {
+		return words.contains(word);
+	}
+
+	public boolean isAPalindrome(String word) {
+		// String a = "";
+		StringBuilder word2 = new StringBuilder(word);
+//		word2.reverse();
+//		int length = word.length();
+//		for (int i = length - 1; i > 0; i--) {
+//			word2 = word2 + word.charAt(i);
+		if (word2.reverse().toString() == (word)) {
+			// return true;
+			System.out.println("The string " + word + " is a palindrome");
+		}
+
+		return true;
+	}
+
 }
