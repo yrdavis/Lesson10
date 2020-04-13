ea58c078d0bbb1bce7938e82dfbe01e105a6e63f
yrdavis <yrd1@hotmail.com>
2020-04-12 22:09:42 -0700

    Test file for Palindrome file.

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
+
 	Palindrome testPalindrome = new Palindrome();
 
 	@Test
 	void testLoadWords() {
 		assertTrue(testPalindrome.loadWords().contains("apple"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	void testWordExists() {
+		assertTrue(testPalindrome.wordExists("kayak"));
+	}
+
 	// 3. Test that a word is a palindrome
+	void testWordPalindrome() {
+		assertTrue(testPalindrome.isAPalindrome("kayak"));
+	}
 
 }
